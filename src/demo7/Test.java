e8d839f32d2bd04558d62d776ed534fb055fefbc
ghStk <ghstk@qq.com>
2020-05-08 20:35:09 +0800

    #7 reverse

--- src/demo7/Test.java
+++ src/demo7/Test.java
@@ -5,7 +5,7 @@ import test.Generate;
 public class Test {
     public static void main(String[] args) {
         Solution slt = new Solution();
-        for (int x : Generate.getInt(10)) {
+        for (int x : Generate.getInt(6)) {
             System.out.print(x+" ");
             System.out.println(slt.reverse(x));
         }
