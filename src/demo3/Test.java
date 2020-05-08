aba757fae5dd078a65bc941eabf3772c489eae0e
ghStk <ghstk@qq.com>
2020-05-08 14:15:03 +0800

    nul

--- src/demo3/Test.java
+++ src/demo3/Test.java
@@ -4,11 +4,12 @@ import test.Generate;
 
 public class Test {
     public static void main(String[] args) {
-        Solution slt = new Solution();
-        for (int i = 0; i < 1; i++) {
+        Solution2 slt = new Solution2();
+        for (int i = 0; i < 10; i++) {
             String s = Generate.getString(10);
             System.out.print(s+" res=");
-            System.out.println(slt.lengthOfLongestSubstring("abcabcbb"));
+//            System.out.println(slt.lengthOfLongestSubstring("dvdf"));
+            System.out.println(slt.lengthOfLongestSubstring(s));
         }
     }
 }
