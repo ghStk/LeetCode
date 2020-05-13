a405fef3c92c36cdf1633dd2a043f6cf13217940
ghStk <ghstk@qq.com>
2020-05-13 13:26:53 +0800

    PC @2020-5-13 13:26:48

---
+++ src/demo12/test.java
@@ -0,0 +1,17 @@
+package demo12;
+
+import test.Generate;
+
+/**
+ * 12.给定一个整数，将其转为罗马数字。输入确保在 1 到 3999 的范围内。
+ */
+public class test {
+    public static void main(String[] args) {
+        Solution slt = new Solution();
+//        System.out.println(slt.intToRoman(3));
+//        System.out.println(slt.intToRoman(4));
+//        System.out.println(slt.intToRoman(9));
+//        System.out.println(slt.intToRoman(58));
+        System.out.println(slt.intToRoman(1994));
+    }
+}
