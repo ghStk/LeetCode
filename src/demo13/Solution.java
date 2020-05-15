abd5969836b9dccdbcf5c388c57419d30a20cc00
ghStk <ghstk@qq.com>
2020-05-15 17:16:58 +0800

    PC @2020-5-15 17:16:52

--- src/demo13/Solution.java
+++ src/demo13/Solution.java
@@ -1,7 +1,14 @@
 package demo13;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ */
 class Solution {
     public int romanToInt(String s) {
+        Map<Character,Integer> map= new HashMap<Character,Integer>();
         int length = s.length();
         char tmp;
         for (int i = 0; i < length; i++) {
