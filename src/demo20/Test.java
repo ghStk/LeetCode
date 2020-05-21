5c367fb544afe61cad4dc37548bc6e23beacbbe8
ghStk <ghstk@qq.com>
2020-05-21 15:35:36 +0800

    Laptop @2020-5-21 15:35:27

---
+++ src/demo20/Test.java
@@ -0,0 +1,13 @@
+package demo20;
+
+import java.util.LinkedList;
+
+public class Test {
+    public static void main(String[] args) {
+        Solution slt = new Solution();
+        String s = "[({{][}})]";
+//        System.out.println(slt.isValid(s));
+        LinkedList<Integer> a = new LinkedList<>();
+        System.out.println(a.getLast());
+    }
+}
